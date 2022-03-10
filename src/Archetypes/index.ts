5bc608918e0380fecd3adcd157ab49e8bc39fb1c
guizarolho <guimfontes7@gmail.com>
2022-03-10 19:39:08 -0300

    Interface fighter

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -2,7 +2,7 @@ import Archetype from './Archetype';
 import Mage from './Mage';
 import Necromancer from './Necromancer';
 import Ranger from './Ranger';
-import Warrior from './Warriors';
+import Warrior from './Warrior';
 
 export { Mage, Necromancer, Ranger, Warrior };
 
