761334340480a6f21cfbe250c11c1acaa6e44af0
guizarolho <guimfontes7@gmail.com>
2022-03-10 18:27:00 -0300

    Generate races

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,27 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  private _maxLifePoints: number;
+  private static instances = 0;
+  
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+    Halfling.addToCount();
+  }
+
+  static addToCount() {
+    this.instances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;  
+  }
+
+  static createdRacesInstances():number {
+    return this.instances;
+  }
+}
--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -13,7 +13,7 @@ export default abstract class Race {
   get name(): string { return this._name; }
   get dexterity(): number { return this._dexterity; }
 
-  static createdRacesInstances(): number {
+  static createdRacesInstances() {
     throw new Error('Not implemented');
   }
 
