40e352b20d21018657f5bf09fc85d35085fb68cc
guizarolho <guimfontes7@gmail.com>
2022-03-11 15:45:41 -0300

    Cria classe PVP

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,16 +1,24 @@
-import Character from '../Character';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
   private _c1;
   private _c2;
-  constructor(c1: Character, c2: Character) {
+  constructor(c1: Fighter, c2: Fighter) {
     super(c1);
     this._c1 = c1;
     this._c2 = c2;
   }
   
+  tradeBlows() {
+    this._c1.attack(this._c2);
+    this._c2.attack(this._c1);
+  }
+
   fight(): number {
-    return this.player.lifePoints === -1 ? -1 : 1;
+    while (this._c1.lifePoints > -1 && this._c2.lifePoints > -1) {
+      this.tradeBlows();
+    }
+    return this._c1.lifePoints === -1 ? -1 : 1;
   }
 }
