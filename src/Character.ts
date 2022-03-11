6ed4fa66606ad35ced9605d653bade4949292ea1
guizarolho <guimfontes7@gmail.com>
2022-03-11 17:52:23 -0300

    index

--- src/Character.ts
+++ src/Character.ts
@@ -58,9 +58,13 @@ export default class Character implements Fighter {
 
   levelUp(): void {
     this._maxLifePoints += getRandomInt(1, 10);
+    if (this._maxLifePoints > this.race.maxLifePoints) {
+      this._maxLifePoints = this.race.maxLifePoints;
+    }
     this._strength += getRandomInt(1, 10);
     this._dexterity += getRandomInt(1, 10);
     this._defense += getRandomInt(1, 10);
     this._energy.amount = 10;
+    this._lifePoints = this._maxLifePoints;
   }
 }
